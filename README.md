88f66b517fd782e078d01b97462a52360e62afe5
DJ Spiess <dspiess@nso.edu>
2015-09-16 10:03:16 -0600

    added readment and license

---
+++ README.md
@@ -0,0 +1,31 @@
+# Understand Swift 2.0 guard statements in iOS 9
+
+The purpose of this repository is to supplement the video with code, and to provide a simple starting point for using guard statements in your app code.
+
+The “Understand Swift 2.0 guard statements in iOS 9” video demonstrates using the guard statement to validate a user registration form.
+
+The video uses Swift 2.0 and iOS 9
+
+You can find more iOS videos on [the DeegeU channel](http://www.deegeu.com/subscribe)
+
+## Getting started
+
+The code is a self contained Swift project that can be loaded into XCode.
+
+This code is written and compiled for Swift 2.0.
+
+## Getting help
+
+If you are having troubles getting the source code, please see [Getting the source code](http://www.deegeu.com/getting-the-source-code/) or the video [How to get code from GitHub](http://www.deegeu.com/videos/how-to-get-code-from-github/)  
+
+## License
+
+MIT: http://rem.mit-license.org
+
+## Feedback
+
+Any feedback is welcome. You can contact me at dj at deegeu.com, the [Facebook page](https://www.facebook.com/deegeu.programming.tutorials), the [Google+ page](https://plus.google.com/+Deegeu-programming-tutorials/posts) or on [DeegeU.com](http://www.deegeu.com).
+
+## Contributions
+
+If you wish to contribute to any issues you find in the source code, please issue a pull request.
--- ios-swift-guardTests/ViewController.swift
+++ ios-swift-guardTests/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameEdit: UITextField!
+    
+    @IBOutlet weak var emailEdit: UITextField!
+    
+    @IBOutlet weak var passwordEdit: UITextField!
+    
+    @IBOutlet weak var confirmEdit: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,89 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func registerOnTouch(sender: UIButton) {
+        if let name = nameEdit.text {
+            if let email = emailEdit.text {
+                if let password = passwordEdit.text {
+                    if let confirm = confirmEdit.text {
+                        if (password == confirm) {
+                            registerUser(name, email, password)
+                        }
+                    } else {
+                        print("Bad confirm")
+                    }
+                } else {
+                    print("Bad password")
+                }
+            } else {
+                print("Bad email")
+            }
+        } else {
+            print("Bad name")
+        }
+    }
+    
+    @IBAction func registerOnTouch2(sender: UIButton) {
+        if let name = nameEdit.text where name == "" {
+            print("Bad name")
+            return
+        }
+        
+        if let email = emailEdit.text where email == "" {
+            print("Bad email")
+            return
+            
+        }
+        
+        if let password = passwordEdit.text where password == "",
+           let confirm = confirmEdit.text where confirm != password {
+            print("Bad password")
+            return
+        }
+        
+        // Have to force unwrapping again!?!?
+        registerUser(nameEdit.text!, emailEdit.text!, passwordEdit.text!)
+    }
+
+    @IBAction func registerOnTouch3(sender: UIButton) {
+        guard let name = nameEdit.text where name != "" else {
+            print("Bad name")
+            return
+        }
+        
+        guard let email = emailEdit.text where email != "" else {
+            print("Bad email")
+            return
+            
+        }
+        
+        guard let password = passwordEdit.text where password != "",
+            let confirm = confirmEdit.text where confirm == password else {
+                print("Bad password")
+                return
+        }
+        
+        // We can use the unwrapped variables!!
+        registerUser(name, email, password)
+    }
+    
+    @IBAction func registerOnTouch4(sender: UIButton) {
+        guard let name = nameEdit.text where name != "",
+              let email = emailEdit.text where email != "",
+              let password = passwordEdit.text where password != "",
+              let confirm = confirmEdit.text where confirm == password else {
+            print("Bad input")
+            return
+        }
+        
+        // We can use the unwrapped variables!!
+        registerUser(name, email, password)
+    }
+
+    
+    func registerUser(username :String, _ email: String, _ password: String) -> Bool {
+        return true
+    }
 
 }
 
